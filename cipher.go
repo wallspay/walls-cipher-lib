f6f83fab2f76790fb6d906b3da5fe8008f1ac72f
nkenchor <nkenchor@osemeke.com>
2023-10-16 16:00:40 +0100

    First commit

---
+++ cipher.go
@@ -0,0 +1,76 @@
+package wallscipherlib
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+)
+
+type CypherLibrary interface {
+	Encrypt(plaintext []byte) (string, error)
+	Decrypt(ciphertext string) (string, error)
+}
+
+type AESGCMLibrary struct {
+	key []byte
+}
+
+func NewAESGCMLibrary(keyString string) *AESGCMLibrary {
+	return &AESGCMLibrary{key: []byte(keyString)}
+}
+
+func (l *AESGCMLibrary) Encrypt(plaintext []byte) (string, error) {
+	block, err := aes.NewCipher(l.key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func (l *AESGCMLibrary) Decrypt(encryptedText string) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(encryptedText)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(l.key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	if len(ciphertext) < gcm.NonceSize() {
+		return "", errors.New("ciphertext too short")
+	}
+
+	nonce := ciphertext[:gcm.NonceSize()]
+	ciphertext = ciphertext[gcm.NonceSize():]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wallspay/walls-cipher-lib
+
+go 1.20
